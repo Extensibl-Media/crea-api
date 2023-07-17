b6efb447db0e4610880f0b2d1924604a1a606dd0
Richard <richardsprins@gmail.com>
2023-07-17 11:45:23 -0600

    comment out API routes

--- src/api.ts
+++ src/api.ts
@@ -87,28 +87,28 @@ app.use('/api/v1', api);
  */
 
 // Get All CREA Data
-api.get('/crea-raw-data', async (req, res) => {
-  try {
-    const memberToken: CREAAccessToken = await getCREAToken('member');
-    const nationalToken: CREAAccessToken = await getCREAToken('national');
+// api.get('/crea-raw-data', async (req, res) => {
+//   try {
+//     const memberToken: CREAAccessToken = await getCREAToken('member');
+//     const nationalToken: CREAAccessToken = await getCREAToken('national');
 
-    const memberListings = await getCREAListingsData(memberToken);
-    const nationalListings = await getCREAListingsData(nationalToken);
+//     const memberListings = await getCREAListingsData(memberToken);
+//     const nationalListings = await getCREAListingsData(nationalToken);
 
-    const poolIds = new Set(
-      nationalListings.map((listing) => listing.ListingKey)
-    );
-    const finalData = [
-      ...nationalListings,
-      ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
-    ];
+//     const poolIds = new Set(
+//       nationalListings.map((listing) => listing.ListingKey)
+//     );
+//     const finalData = [
+//       ...nationalListings,
+//       ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
+//     ];
 
-    return res.status(200).json({ finalData, count: finalData.length });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).end();
-  }
-});
+//     return res.status(200).json({ finalData, count: finalData.length });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).end();
+//   }
+// });
 
 // Get all webflow CMS data for site
 api.get('/webflow-raw-data', async (req, res) => {
@@ -122,103 +122,104 @@ api.get('/webflow-raw-data', async (req, res) => {
 });
 
 // Clean up CMS
-api.get('/clean-data', async (req, res) => {
-  try {
-    const memberToken: CREAAccessToken = await getCREAToken('member');
-    const nationalToken: CREAAccessToken = await getCREAToken('national');
+// api.get('/clean-data', async (req, res) => {
+//   try {
+//     const memberToken: CREAAccessToken = await getCREAToken('member');
+//     const nationalToken: CREAAccessToken = await getCREAToken('national');
 
-    const memberListings = await getCREAListingsData(memberToken);
-    const nationalListings = await getCREAListingsData(nationalToken);
+//     const memberListings = await getCREAListingsData(memberToken);
+//     const nationalListings = await getCREAListingsData(nationalToken);
 
-    const poolIds = new Set(
-      nationalListings.map((listing) => listing.ListingKey)
-    );
-    const finalData = [
-      ...nationalListings,
-      ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
-    ];
-    const {
-      collection: { items }
-    }: { collection: { items: RawCMSFields[] } } = await getWebflowData();
+//     const poolIds = new Set(
+//       nationalListings.map((listing) => listing.ListingKey)
+//     );
+//     const finalData = [
+//       ...nationalListings,
+//       ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
+//     ];
+//     const {
+//       collection: { items }
+//     }: { collection: { items: RawCMSFields[] } } = await getWebflowData();
 
-    const result = await removeObsoleteCMSItems(finalData, items);
-    return res.status(200).json({ success: true, data: result });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).end();
-  }
-});
+//     const result = await removeObsoleteCMSItems(finalData, items);
+//     return res.status(200).json({ success: true, data: result });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).end();
+//   }
+// });
 
 // Manually sync CMS with CREA data
-api.get('/push-data', async (req, res) => {
-  try {
-    const memberToken: CREAAccessToken = await getCREAToken('member');
-    const nationalToken: CREAAccessToken = await getCREAToken('national');
+// api.get('/push-data', async (req, res) => {
+//   try {
+//     const memberToken: CREAAccessToken = await getCREAToken('member');
+//     const nationalToken: CREAAccessToken = await getCREAToken('national');
 
-    const memberListings = await getCREAListingsData(memberToken);
-    const nationalListings = await getCREAListingsData(nationalToken);
+//     const memberListings = await getCREAListingsData(memberToken);
+//     const nationalListings = await getCREAListingsData(nationalToken);
 
-    const poolIds = new Set(
-      nationalListings.map((listing) => listing.ListingKey)
-    );
-    const finalData = [
-      ...nationalListings,
-      ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
-    ];
+//     const poolIds = new Set(
+//       nationalListings.map((listing) => listing.ListingKey)
+//     );
+//     const finalData = [
+//       ...nationalListings,
+//       ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
+//     ];
 
-    const {
-      collection: { items }
-    }: { collection: { items: RawCMSFields[] } } = await getWebflowData();
-    const result = await syncDataWithCMS(finalData, items);
-    return res.status(200).json({ success: true, data: result });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).end();
-  }
-});
+//     const {
+//       collection: { items }
+//     }: { collection: { items: RawCMSFields[] } } = await getWebflowData();
+//     const result = await syncDataWithCMS(finalData, items);
+//     return res.status(200).json({ success: true, data: result });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).end();
+//   }
+// });
 
-api.get('/full-sync', async (req, res) => {
-  console.log('Running scheduled job!');
-  try {
-    // Get CREA Listing Data
-    const memberToken: CREAAccessToken = await getCREAToken('member');
-    const nationalToken: CREAAccessToken = await getCREAToken('national');
+// Manually run full job
+// api.get('/full-sync', async (req, res) => {
+//   console.log('Running scheduled job!');
+//   try {
+//     // Get CREA Listing Data
+//     const memberToken: CREAAccessToken = await getCREAToken('member');
+//     const nationalToken: CREAAccessToken = await getCREAToken('national');
 
-    const memberListings = await getCREAListingsData(memberToken);
-    const nationalListings = await getCREAListingsData(nationalToken);
+//     const memberListings = await getCREAListingsData(memberToken);
+//     const nationalListings = await getCREAListingsData(nationalToken);
 
-    const poolIds = new Set(
-      nationalListings.map((listing) => listing.ListingKey)
-    );
-    const finalData = [
-      ...nationalListings,
-      ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
-    ];
+//     const poolIds = new Set(
+//       nationalListings.map((listing) => listing.ListingKey)
+//     );
+//     const finalData = [
+//       ...nationalListings,
+//       ...memberListings.filter((listing) => !poolIds.has(listing.ListingKey))
+//     ];
 
-    // Get relevant Webflow data
-    const {
-      collection: { items }
-    }: { collection: { items: RawCMSFields[] } } = await getWebflowData();
+//     // Get relevant Webflow data
+//     const {
+//       collection: { items }
+//     }: { collection: { items: RawCMSFields[] } } = await getWebflowData();
 
-    // Sync Webflow CMS with listing data and get returned new listing collection items
-    const newItems = await syncDataWithCMS(finalData, items);
+//     // Sync Webflow CMS with listing data and get returned new listing collection items
+//     const newItems = await syncDataWithCMS(finalData, items);
 
-    // Clean up Webflow CMS of any listings no linger in the CREA database
-    await removeObsoleteCMSItems(finalData, items);
+//     // Clean up Webflow CMS of any listings no linger in the CREA database
+//     await removeObsoleteCMSItems(finalData, items);
 
-    const itemIds = newItems.map((item) => item?._id);
-    console.log(itemIds);
+//     const itemIds = newItems.map((item) => item?._id);
+//     console.log(itemIds);
 
-    // Publish new CMS items to site
-    await publishCMSData(itemIds);
+//     // Publish new CMS items to site
+//     await publishCMSData(itemIds);
 
-    // Finally, publish site so all new listings are live
-    await publishSite();
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    console.log(`Error running job`, error);
-    return res.status(500).end();
-  } finally {
-    console.log('Scheduled job complete.');
-  }
-});
+//     // Finally, publish site so all new listings are live
+//     await publishSite();
+//     return res.status(200).json({ success: true });
+//   } catch (error) {
+//     console.log(`Error running job`, error);
+//     return res.status(500).end();
+//   } finally {
+//     console.log('Scheduled job complete.');
+//   }
+// });
